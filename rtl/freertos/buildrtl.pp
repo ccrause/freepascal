8fc93c7584a8437decbdafd355a9e20c01e79491
ccrause <christo.crause@gmail.com>
2020-04-10 09:28:44 +0200

    Laarus project to edit RTL files with.

---
+++ rtl/freertos/buildrtl.pp
@@ -0,0 +1,18 @@
+{ This unit is only used to edit the rtl with lazarus }
+unit buildrtl;
+
+  interface
+
+    uses
+      system, unixtype, ctypes, baseunix, strings, objpas, macpas, syscall, unixutil, heapmgr,
+      fpintres, heaptrc, lineinfo, lnfodwrf,
+      termio, crt, sysutils, typinfo, math, matrix, varutils,
+      charset, ucomplex, getopts,
+      errors, sockets, gpm, ipc, serial, terminfo,
+      video, mouse, keyboard, variants, types, dateutils, sysconst, fmtbcd,
+      cthreads, classes, fgl, convutils, stdconvs, strutils, rtlconsts, dos, objects, cwstring, clocale,
+      exeinfo;
+
+  implementation
+
+end.
