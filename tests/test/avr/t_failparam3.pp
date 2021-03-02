4c43c7e1d38621f60da45b72e8d58030ca8c6008
ccrause <christo.crause@gmail.com>
2021-03-02 08:06:51 +0200

    Section type and normal type pass by value parameters are indistinguishable as the section information is redundant after a value has been loaded.

---
+++ tests/test/avr/t_failparam3.pp
@@ -0,0 +1,28 @@
+{ %cpu=avr }
+{ %target=embedded }
+{ %fail }
+
+program t_failparam2;
+
+type
+  myword_progmem = type word; section '.progmem';
+
+procedure test(w: myword_progmem);
+begin
+  Halt(low(w));
+end;
+
+procedure test(w: word);
+begin
+  Halt(low(w));
+end;
+
+var
+  w_eep: word = $1234; section '.eeprom';
+  
+begin
+  { Should fail with: Error: Can't determine which overloaded function to call. 
+    Section information has no meaning when passing a value. }
+  test(w_eep);
+end.
+
---
+++ tests/test/avr/t_valueparamtest1.pp
@@ -0,0 +1,33 @@
+{ %cpu=avr }
+{ %target=embedded }
+
+program t_valueparamtest1;
+
+type
+  Byte_eeprom = type byte; section '.eeprom';
+  PByte_eeprom = type PByte; section '.eeprom';
+
+var
+  expectedResult: byte;
+
+procedure proc(b1: byte);
+begin
+  if b1 <> expectedResult then
+    halt(1);
+end;
+
+const
+  b_eep_value = $12;
+  b_value = $23;
+
+var
+  b_eep: byte = b_eep_value; section '.eeprom';
+  b: byte = b_value;
+
+begin
+  expectedResult := b_eep_value;
+  proc(b_eep);
+
+  expectedResult := b_value;
+  proc(b);
+end.
