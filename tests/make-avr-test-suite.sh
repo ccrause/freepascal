7ff8e13ae174762565b758fe2c0128a3da3c0c58
ccrause <christo.crause@gmail.com>
2021-03-05 15:54:00 +0200

    Removed verbose options from make command

--- tests/make-avr-test-suite.sh
+++ tests/make-avr-test-suite.sh
@@ -10,11 +10,13 @@ DriverOptions="-vewhi"
 TestCompiler=~/fpc/cc/compiler/avr/pp
 TestSubArch=avr5
 # TestController=atmega328p
-TestOptions="-n @/home/christo/fpc/cc/fpc.cfg -XPavr- -Wpavrsim -vewhi -gw3"
+TestOptions="-n @~/fpc/cc/fpc.cfg -XPavr- -Wpavrsim -vewhi -gw3"
 #TestOptions="-XPavr- -vt"
 
 cd $TestFPCSource/tests
 
 #make distclean TEST_DELTEMP=1 FPC=$DriverCompiler TEST_FPC=$TestCompiler V=1 TEST_SUBARCH=$TestSubArch TEST_OPT= TEST_VERBOSE=1 FPCMAKE=$TestFpcMake
 
-make alltest TEST_DELTEMP=1 FPC=$DriverCompiler TEST_FPC=$TestCompiler V=1 TEST_SUBARCH=$TestSubArch TEST_OPT="$TestOptions" TEST_VERBOSE=1 TEST_CPU_TARGET=avr TEST_OS_TARGET=embedded FPCMAKE=$TestFpcMake EMULATOR=/home/christo/LazProjs/fp-avrsim-cc/avrsim TEST_TIMEOUT=1 CHUNKS=20
+# include TEST_VERBOSE below to print compiler command line and test build options during test run
+
+make alltest TEST_DELTEMP=1 FPC=$DriverCompiler TEST_FPC=$TestCompiler TEST_SUBARCH=$TestSubArch TEST_OPT="$TestOptions" TEST_CPU_TARGET=avr TEST_OS_TARGET=embedded FPCMAKE=$TestFpcMake EMULATOR=/home/christo/LazProjs/fp-avrsim-cc/avrsim TEST_TIMEOUT=1 CHUNKS=20
