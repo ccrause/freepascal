1937b61e64b2b716d104d484db0af92927fd9f6e
ccrause <christo.crause@gmail.com>
2021-03-05 15:59:02 +0200

    More test cases

---
+++ tests/test/avr/u_mixedoverload.pas
@@ -0,0 +1,24 @@
+unit u_mixedoverload;
+
+interface
+
+type
+  Byte_e = type Byte; section '.eeprom';
+
+procedure writeTest(var b1: Byte); overload;
+procedure writeTest(var b1: Byte_e); overload;
+
+implementation
+
+procedure writeTest(var b1: Byte); overload;
+begin
+  b1 := 123;
+end;
+
+procedure writeTest(var b1: Byte_e); overload;
+begin
+  b1 := 234;
+end;
+
+end.
+
