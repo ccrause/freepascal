fe03ddab50d0f188137fe1ff7cc7871c320a0932
ccrause <christo.crause@gmail.com>
2021-03-06 12:07:14 +0200

    Use local branch version of fpcmake to properly rebuild packages.

--- tests/make-avr-test-suite.sh
+++ tests/make-avr-test-suite.sh
@@ -5,17 +5,21 @@ TestFPCSource=~/fpc/cc
 #DriverCompiler uses global fpc.cfg so make sure everything is updated, compiled and installed before using it
 DriverCompiler=~/fpc/installs/lib/fpc/3.2.0/ppcx64
 #DriverCompiler=/home/christo/fpc/cc/compiler/ppcx64
-DriverFpcMake=~/fpc/installs/bin/fpcmake
+
+DriverFpcMake=$(TestFPCSource)/utils/fpcm/bin/x86_64-linux/fpcmake
+# or call export in terminal: export FPCFPMAKE=~/fpc/cc/utils/fpcm/bin/x86_64-linux/fpcmake 
+
 DriverOptions="-vewhi"
 TestCompiler=~/fpc/cc/compiler/avr/pp
 TestSubArch=avr5
 # TestController=atmega328p
-TestOptions="-n @~/fpc/cc/fpc.cfg -XPavr- -Wpavrsim -vewhi -gw3"
-#TestOptions="-XPavr- -vt"
+TestOptions="-n @~/fpc/cc/fpc.cfg -XPavr- -Wpavrsim -vewhi"
 
 cd $TestFPCSource/tests
 
-#make distclean TEST_DELTEMP=1 FPC=$DriverCompiler TEST_FPC=$TestCompiler V=1 TEST_SUBARCH=$TestSubArch TEST_OPT= TEST_VERBOSE=1 FPCMAKE=$TestFpcMake
+export FPCFPMAKE=$(DriverFpcMake)
+
+make distclean TEST_DELTEMP=1 FPC=$DriverCompiler TEST_FPC=$TestCompiler V=1 TEST_SUBARCH=$TestSubArch TEST_OPT= TEST_VERBOSE=1 #FPCFPMAKE=$DriverFpcMake
 
 # include TEST_VERBOSE below to print compiler command line and test build options during test run
 
