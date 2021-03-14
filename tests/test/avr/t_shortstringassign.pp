8f7b88628f0186e471c8cff187c54351609e34af
ccrause <christo.crause@gmail.com>
2021-03-14 18:36:06 +0200

    Include testing of progmem

--- tests/test/avr/t_shortstringassign.pp
+++ tests/test/avr/t_shortstringassign.pp
@@ -5,14 +5,19 @@ program t_shortstringassign;
 
 var
   se: shortstring = 'qweryt'; section '.eeprom';
+  sf: shortstring = 'trewq'; section '.progmem';
   s: shortstring = '';
 
 begin
   s := se;
-
   if s[1] <> 'q' then
     halt(1);
-
   if length(s) <> 6 then
     halt(2);
+
+  s := sf;
+  if s[1] <> 't' then
+    halt(3);
+  if length(s) <> 5 then
+    halt(4);
 end.
