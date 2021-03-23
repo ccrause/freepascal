75c91f079d1f47243992e07b2ae7a601e24fe3f5
ccrause <christo.crause@gmail.com>
2021-03-23 22:17:00 +0200

    Tweak test to focus on correct storage of literal in progmem

--- tests/test/avr/t_stringliteral.pp
+++ tests/test/avr/t_stringliteral.pp
@@ -15,18 +15,27 @@ begin
   {$stringinprogmem+} // store literal in progmem
   s := '43210';
 
-  { The address of progmem in flash is not fixed.
-    Use address of variable x as pointer to start of progmem }
-  p_p := @x;
-  { The 2nd byte after x should be the first character of the literal }
-  inc(p_p,2);
-  if p_p^ <> '4' then
-    halt(255);
-
+  { Verify length of s }
   if length(s) <> 5 then
     halt(1);
 
-  if s[5] <> '0' then
+  { Verify that literal is stored in progmem.
+    The address of progmem in flash is not fixed.
+    Use address of variable x as pointer to start of progmem }
+  p_p := @x;
+  { The 1st byte after x should be the length of the literal }
+  inc(p_p);
+  if p_p^ <> #5 then
     halt(2);
+
+  { The 2nd byte after x should be the first character of the literal }
+  inc(p_p);
+  if p_p^ <> '4' then
+    halt(3);
+
+  { Check the last character of the literal }
+  inc(p_p,4);
+  if p_p^ <> '0' then
+    halt(4);
 end.
 
