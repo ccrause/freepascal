6aaa2bfd16fbe5bb49ab9b2ab9ad92028ac98257
ccrause <christo.crause@gmail.com>
2021-03-29 15:38:11 +0200

    Simplify test a bit

--- tests/test/avr/t_initsection.pp
+++ tests/test/avr/t_initsection.pp
@@ -5,25 +5,27 @@ program t_initsection;
 
 const
   init1_const = $55;
-  init3_const = $AA;
 
 var
-  b1, b3: byte; section '.noinit';
+  b: byte;
 
+// This should be inserted before _FPC_init_zeroreg_SP
 procedure init1Proc; noreturn; section '.init1';
 begin
-  b1 := init1_const;
+  b := init1_const;
 end;
 
+// This should be inserted after _FPC_init_zeroreg_SP
+// and before _FPC_copy_data
+// b should still have its previously assigned value
 procedure init3Proc; noreturn; section '.init3';
 begin
-  b3 := init3_const;
+  if b <> init1_const then
+    Halt(1);
 end;
 
 begin
-  if b1 <> init1_const then
-    halt(1);
-
-  if b3 <> init3_const then
-    halt(3);
-end. 
+  // _FPC_copy_data should clear b before entering PASCALMAIN
+  if b > 0 then
+    halt(2);
+end.
